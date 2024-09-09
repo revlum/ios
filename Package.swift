3ba8a2af230c9ab8d83a89d639243084d6a80f70
mkovacevic2 <mkovacevic@4ofthem.eu>
2024-09-09 18:08:19 +0200

    update package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "RevlumOfferwallSDK",
             url:"https://github.com/revlum/ios/releases/download/v1.0.0/RevlumOfferwallSDK.xcframework.zip",
-            checksum: "d9aa71dd0be7e5a8fa3006a3e5528debb11fac526f9d57785c3a85a9209f6f20"
+            checksum: "522246bcb322b48114145b318dcf42a04acd725b901e0329f6fc958b1474fbee"
         ),
     ]
 )
