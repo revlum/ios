697eefaaf5100988a1abe52513de997db99394d9
kovaccc91 <matej.kovacevic@speck.agency>
2024-10-11 20:59:51 +0200

    add new version with checksum

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "RevlumOfferwallSDK",
+            name: "RevlumOfferwallSDK", 
             targets: ["RevlumOfferwallSDK"]),
     ],
     targets: [
         .binaryTarget(
             name: "RevlumOfferwallSDK",
-            url:"https://github.com/revlum/ios/releases/download/v1.0.0/RevlumOfferwallSDK.xcframework.zip",
-            checksum: "780bb8ab8c3f08b433953371207e85857e6defa59b4beee3aa378da65cdd0989"
-        ),
+            url: "https://github.com/revlum/ios/releases/download/v1.0.0/RevlumOfferwallSDK.xcframework.zip",
+            checksum: "30bc2d9bc56e09739936201b251365eb2e830d1162b04dcbde3ef9c1057dd43e"
+        )
     ]
 )
